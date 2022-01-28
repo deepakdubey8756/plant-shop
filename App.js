64425459aff241fe09df86912159100967274a21
deepak <dubeydeepak8756@gmail.com>
2022-01-28 15:43:47 +0530

    final commit

--- App.js
+++ App.js
@@ -1,20 +1,53 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import {NavigationContainer} from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack';
 
-export default function App() {
+const Stack = createStackNavigator();
+import HomeScreen from './src/view/screens/HomeScreen';
+import DetailsSceeen from './src/view/screens/DetailsScreen';
+import {StatusBar} from 'react-native';
+import COLORS from './src/consts/colors';
+import SplashScreen from './src/view/screens/SplashScreen';
+import WelcomeHome from './src/view/screens/WelcomeHome';
+import LoginScreen from './src/view/screens/LoginScreen';
+import userData from './src/view/screens/UserData';
+import ShoppingCart from './src/view/screens/ShoppingCart';
+import SignUp from './src/view/screens/SignupScreen';
+import OrderLists from './src/view/screens/OrderLists';
+import HelpDesk from './src/view/screens/HelpDesk';
+import UserDetails from './src/view/screens/UserDetails';
+import AboutUs from './src/view/screens/AboutUs';
+import Checkout from './src/view/screens/Checkout';
+import Congratulation from './src/view/screens/Congratulations';
+import TrackOrder from './src/view/screens/OrderTrack'
+import FlashMessage from "react-native-flash-message";
+import TakeOtp from "./src/view/screens/TakeOtp"
+
+const App = () => {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <StatusBar barStyle="dark-content" backgroundColor={COLORS.green} />
+      <Stack.Navigator screenOptions={{header: () => null}}>
+        <Stack.Screen name="SplashScreen" component={SplashScreen} />
+        <Stack.Screen name="WelcomeHome" component={WelcomeHome} />
+        <Stack.Screen name="shoppingCart" component={ShoppingCart} />
+        <Stack.Screen name="userData" component={userData} />
+        <Stack.Screen name="AboutUs" component={AboutUs} />
+        <Stack.Screen name="helpDesk" component={HelpDesk} />
+        <Stack.Screen name="orderList" component={OrderLists} /> 
+        <Stack.Screen name="orderTrack" component={TrackOrder} /> 
+        <Stack.Screen name="userDetails" component={UserDetails} />
+        <Stack.Screen name="checkOut" component={Checkout} />
+        <Stack.Screen name="congratulations" component={Congratulation} /> 
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Details" component={DetailsSceeen} /> 
+        <Stack.Screen name="signUp" component={SignUp}/>
+        <Stack.Screen name="takeOtp" component={TakeOtp} />
+        <Stack.Screen name="LoginScreen" component={LoginScreen} /> 
+      </Stack.Navigator>
+      <FlashMessage position="top"/>
+    </NavigationContainer>
   );
-}
+};
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
